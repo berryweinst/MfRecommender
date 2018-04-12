1069c20479f908015609c0f1d8f1c17ab22159eb
Berry Weinstein <35626084+berryweinst@users.noreply.github.com>
2018-04-12 21:07:39 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,51 +1,83 @@
-import random
-import math
+import numpy as np
+from data import *
 
 
-def create_user_item_map(ratings_path, movies_path, percent=0.8, sort_ratings=False):
-    users_map = dict()
-    items_map = dict()
-    user_id = 0
-    item_id = 0
-    ratings_data = dict()
+class MFModel(object):
 
-    rating_cnt = 0
-    with open(ratings_path, 'r') as ratings:
-        for line in ratings:
-            line_split = line.split('::')
-            user = int(line_split[0])
-            item = int(line_split[1])
-            rating = float(line_split[2])
-            timestamp = int(line_split[3])
-            if user not in users_map:
-                users_map[user] = user_id
-                user_id += 1
-            if item not in items_map:
-                items_map[item] = item_id
-                item_id += 1
-            rating_cnt += 1
-            if users_map[user] not in ratings_data:
-                ratings_data[users_map[user]] = []
-            ratings_data[users_map[user]].append((items_map[item], rating, timestamp))
+    def __init__(self, rates_arr, hidden_dim, lr, gamma, opechs):
+        """
+        Performs matrix factorization
+        Args
+        1) ratings - rating matrix (as NDarray)
+        2) hidden_dim  - number of latent dimensions (as integer)
+        3) lr - learning rate (as float)
+        4) gamma - regulaizer (as float)
+        5) opechs - as int
+        """
 
-    X_train = dict()
-    X_test = dict()
-    training_items = set()
-    for user_id in ratings_data.keys():
-        if len(ratings_data[user_id]) < 2:
-            continue
-        if sort_ratings: # sort ratings by their timestamp
-            ratings = sorted(ratings_data[user_id], key=lambda x: x[2])
-        else:
-            ratings = random.shuffle([rate for rate in ratings_data[user_id]])
-        t_idx = math.floor(percent * len(ratings))
-        X_train[user_id] = ratings[:t_idx]
-        for rating_item in ratings[:t_idx]:
-            training_items.add(rating_item[0])  # keep list of items in the training set
-        X_test[user_id] = ratings[t_idx:]
+        self.ratings = rates_arr
+        self.num_users, self.num_items = rates_arr.shape
+        self.K = hidden_dim
+        self.lr = lr
+        self.gamma = gamma
+        self.opechs = opechs
 
-    # Better to remove items that are not appear in the training set
-    for user_id, user_rates in X_test.items():
-        X_test[user_id] = [rating for rating in user_rates if rating[0] in training_items]
+    def train(self):
+        # Initialize user and item latent feature matrice
+        self.U = np.random.normal(scale=1. / self.K, size=(self.num_users, self.K))
+        self.V = np.random.normal(scale=1. / self.K, size=(self.num_items, self.K))
 
-    return X_train, X_test
+        # Initialize the biases
+        self.b_u = np.zeros(self.num_users)
+        self.b_v = np.zeros(self.num_items)
+        # self.b = np.mean(self.ratings[np.where(self.ratings != 0)])
+
+        # Create a list of training samples
+        self.samples = [
+            (i, j, self.ratings[i, j])
+            for i in range(self.num_users)
+            for j in range(self.num_items)
+            if self.ratings[i, j] > 0
+        ]
+
+        # Perform stochastic gradient descent for number of iterations
+        training_process = []
+        for i in range(self.opechs):
+            np.random.shuffle(self.samples)
+            self.LearnModelFromDataUsingSGD()
+            rmse = self.RMSE()
+            training_process.append((i, rmse))
+            if (i + 1) % 5 == 0:
+                print("Iteration: %d ; error = %.4f" % (i + 1, rmse))
+
+        return training_process
+
+    def RMSE(self):
+        xs, ys = self.ratings.nonzero()
+        pred = self.b_u[:,np.newaxis] + self.b_v[np.newaxis:,] + self.U.dot(self.V.T)
+        error = 0
+        for x, y in zip(xs, ys):
+            error += pow(self.ratings[x, y] - pred[x, y], 2)
+        return np.sqrt(error / xs.shape[0])
+
+
+
+    def LearnModelFromDataUsingSGD (self):
+        for i, j, rate in self.samples:
+            prediction = self.b_u[i] + self.b_v[j] + self.U[i, :].dot(self.V[j, :].T)
+            e = (rate - prediction)
+
+            # bias update
+            self.b_u[i] += self.lr * (e - self.gamma * self.b_u[i])
+            self.b_v[j] += self.lr * (e - self.gamma * self.b_v[j])
+
+            # user item metrices
+            self.U[i, :] += self.lr * (e * self.V[j, :] - self.gamma * self.U[i, :])
+            self.V[j, :] += self.lr * (e * self.U[i, :] - self.gamma * self.V[j, :])
+
+
+
+if __name__ == '__main__':
+    X_train, X_test = create_user_item_map('../ml-1m/ratings.dat', '', percent=0.8, sort_ratings=True)
+    mf = MFModel(X_train, 5, 0.01, 0.01, 100)
+    res = mf.train()
